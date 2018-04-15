19675b4b9795c4498de51ee6a059711d5091b71a
Thibaut Coutard <thibaut.coutard@epitech.eu>
2018-04-15 22:55:11 +0200

    Initial Commit

---
+++ PushNotificationExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PushNotificationExample
+//
+//  Created by Thibaut Coutard on 15/04/2018.
+//  Copyright © 2018 Thibaut Coutard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
